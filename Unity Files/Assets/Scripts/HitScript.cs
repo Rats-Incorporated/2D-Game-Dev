df85fc27713e64bfc307b0d294bb8d368ccd7d98
EFerrante2002 <169213135+EFerrante2002@users.noreply.github.com>
2025-12-28 15:51:30 -0800

    Title Screen Implementation

--- Unity Files/Assets/Scripts/HitScript.cs
+++ Unity Files/Assets/Scripts/HitScript.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HitScript : MonoBehaviour
 {
     public float damageAmount = 25f;
+    private float mainSceneIndex;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,5 +16,18 @@ public class HitScript : MonoBehaviour
                 enemy.EnemyTakeDamage(damageAmount);
             }
         }
+        // for start screen
+        if (collision.CompareTag("StartButton"))
+        {
+            mainSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(0); //should be the first level?
+        } 
+        if (collision.CompareTag("Text"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
+
+
+
 }
