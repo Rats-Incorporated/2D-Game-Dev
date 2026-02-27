f01105ab4f473d54ea8a840de5b491ae82ded480
vixicat <dustyisntsorusty@gmail.com>
2026-02-27 04:54:52 -0800

    fixed an issue with my jump pad implementation

--- Unity Files/Assets/Scripts/JumpPad.cs
+++ Unity Files/Assets/Scripts/JumpPad.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    float bounce = 20f;
+    public float bounce = 20f;
+    public float cooldown = 0.4f;
+    private float cd_timer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && cd_timer >= cooldown)
         {
+            cd_timer = 0;
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-            rb.linearVelocityY = 0f;
+            if (rb.linearVelocityY < 0)
+            {
+                rb.linearVelocityY = 0f;
+            }
             rb.AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Start()
+    {
+        cd_timer = cooldown;
+    }
+
+    private void Update()
+    {
+        if (cd_timer <= cooldown)
+        {
+            cd_timer += Time.deltaTime;
         }
     }
 }
