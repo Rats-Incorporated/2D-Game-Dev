eaaeb99dab5fabf2fb2ddeb991c02c166db37b6b
handm-cs <handm@oregonstate.edu>
2026-04-19 22:24:39 -0700

    collectables counter

--- Unity Files/Assets/Scripts/CheesePickup.cs
+++ Unity Files/Assets/Scripts/CheesePickup.cs
@@ -4,13 +4,31 @@ using System.Collections;
 public class CheesePickup : MonoBehaviour
 {
 
+    bool preventDuplicate = false;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
 
-
+        if (preventDuplicate == true)
+        {
+            return;
+        }
         if (collision.CompareTag("Player"))
         {
+
+            preventDuplicate = true;
+
             Destroy(gameObject);
+
+
+
+            //collectable count
+            PlayerController pc = collision.GetComponentInParent<PlayerController>();
+
+            if (pc != null)
+            {
+                pc.Logic.AddCollectableCount();
+            }
 
 
 
