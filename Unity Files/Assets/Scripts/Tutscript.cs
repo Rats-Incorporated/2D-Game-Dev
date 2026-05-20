740504c6913a8b1faf6194f339f4fd62609d5275
FerranteOSU <ferrante@engr.oregonstate.edu>
2026-05-19 23:24:42 -0700

    Revert "Shuriken nerf + Tutorial additions"

--- Unity Files/Assets/Scripts/Tutscript.cs
+++ Unity Files/Assets/Scripts/Tutscript.cs
@@ -4,12 +4,10 @@ using UnityEngine.Events;
 public class TutorialTrigger : MonoBehaviour
 {
     public UnityEvent onTriggered;
-    public GameObject Tutrat;
 
     public void Trigger()
     {
         onTriggered?.Invoke();
         Destroy(gameObject);
-        Tutrat.SetActive(false);
     }
 }
